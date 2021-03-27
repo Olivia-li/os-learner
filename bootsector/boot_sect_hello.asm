10a8fa481f8c2ad7d175b82a5f7a4350bc131eac
Olivia Li <oliviali.ouyang@gmail.com>
2021-03-27 00:23:18 -0700

    hello world

--- bootsector/boot_sect_hello.asm
+++ bootsector/boot_sect_hello.asm
@@ -1,16 +1,29 @@
-mov ah, 0x0e ; tty mode
+; tty mode
+mov ah, 0x0e
 mov al, 'H'
 int 0x10
 mov al, 'e'
 int 0x10
 mov al, 'l'
 int 0x10
-int 0x10 ; 'l' is still on al, remember?
+int 0x10 
 mov al, 'o'
 int 0x10
+mov al, ' '
+int 0x10
+mov al, 'W'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'd'
+int 0x10
 
-jmp $ ; jump to current address = infinite loop
+; infinite loop
+jmp $ 
 
-; padding and magic number
 times 510 - ($-$$) db 0
 dw 0xaa55 
